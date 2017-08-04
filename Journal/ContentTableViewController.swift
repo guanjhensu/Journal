34c933bcf419debbdd3f98c6ca99f448708ba96b
guanjhensu <guanjhensu@gmail.com>
2017-08-04 13:32:52 +0800

    save photo

--- Journal/ContentTableViewController.swift
+++ Journal/ContentTableViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import Photos
 
-class ContentTableViewController: UITableViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate  {
+class ContentTableViewController: UITableViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate, UITextViewDelegate {
 
     // MARK: Property
     
@@ -22,22 +23,45 @@ class ContentTableViewController: UITableViewController, UINavigationControllerD
     
     var imagePicker = UIImagePickerController()
     
+    var localImageId: String!
+    
     // MARK: View Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        contentTextView.delegate = self
+        
+        contentTextView.tintColor = UIColor.black
+        
+        titleTextField.tintColor = UIColor.black
 
     }
     
-    // cell height
+    // MARK: Cell height
+
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return UIScreen.main.bounds.size.height
     }
+    
+    // MARK: UITextViewDelegate
+
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        if text == "\n" {
+            view.endEditing(true)
+            return false
+        }
+        return true
+    }
+
+    // MARK: Dismiss to main page
 
     @IBAction func dismissToMainPage(_ sender: Any) {
         
         self.dismiss(animated: true, completion: nil)
     }
+
+    // MARK: Photo
 
     @IBAction func pickImage(_ sender: Any) {
         
@@ -108,7 +132,75 @@ class ContentTableViewController: UITableViewController, UINavigationControllerD
         self.dismiss(animated: true, completion: nil)
     }
     
+    // MARK: Save
+    
     @IBAction func saveMyPost(_ sender: Any) {
+    
+        let image = myImageView.image!
+
+//        UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+        
+        PHPhotoLibrary.shared().performChanges({
+            
+            let result = PHAssetChangeRequest.creationRequestForAsset(from: image)
+            
+            let assetPlaceholder = result.placeholderForCreatedAsset
+
+            self.localImageId = assetPlaceholder?.localIdentifier
+        
+        }) { (isSuccess: Bool, error: Error?) in
+            
+            if isSuccess {
+                
+                let alert = UIAlertController(title: "Saved!", message: "Your image has been saved to album.", preferredStyle: .alert)
+                
+                alert.addAction(UIAlertAction(title: "OK", style: .default))
+                
+                self.present(alert, animated: true)
+                
+                let assetResult = PHAsset.fetchAssets(withLocalIdentifiers: [self.localImageId], options: nil)
+                
+                let asset = assetResult[0]
+                
+                let options = PHContentEditingInputRequestOptions()
+                
+                options.canHandleAdjustmentData = {(adjustmeta: PHAdjustmentData) -> Bool in
+                    
+                    return true
+                }
+
+                asset.requestContentEditingInput(with: options, completionHandler: { (contentEditingInput: PHContentEditingInput?, _: [AnyHashable : Any]) in
+                    
+                    print("imageURL：", contentEditingInput!.fullSizeImageURL!)})
+                
+                
+                
+                
+                // get image
+                
+                //获取保存的原图
+                PHImageManager.default().requestImage(for: asset,
+                                                      targetSize: PHImageManagerMaximumSize, contentMode: .aspectFit,
+                                                      options: nil, resultHandler: { (image, _:[AnyHashable : Any]?) in
+                                                        print("获取原图成功：\(image)")
+                })
+                //获取保存的缩略图
+                PHImageManager.default().requestImage(for: asset,
+                                                      targetSize: CGSize(width:100, height:100), contentMode: .aspectFit,
+                                                      options: nil, resultHandler: { (image, _:[AnyHashable : Any]?) in
+                                                        print("获取缩略图成功：\(image)")
+                })
+                
+            } else {
+                
+                let alert = UIAlertController(title: "Save error", message: error?.localizedDescription, preferredStyle: .alert)
+                
+                alert.addAction(UIAlertAction(title: "OK", style: .default))
+                
+                self.present(alert, animated: true)
+            }
+        }
+        
     }
     
 }
